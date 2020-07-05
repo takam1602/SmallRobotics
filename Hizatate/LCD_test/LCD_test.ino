f759a6364fb532e6ee5e9ef75a66df726b32c52c
Tomoaki-HIZATATE <tomo_hiza@bpe.agr.hokudai.ac.jp>
2020-07-05 18:47:35 +0900

    LCD表示のテスト

---
+++ Hizatate/LCD_test/LCD_test.ino
@@ -0,0 +1,16 @@
+
+#include <LiquidCrystal.h>
+
+// rs:7 en:8 d4:9 d5:10 d6:11 d7:12　に対応
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+void setup() {
+// LCDの行と列を設定
+lcd.begin(16, 2);
+lcd.clear();
+lcd.setCursor(0, 0);
+lcd.print("hello,world");
+}
+
+void loop() {
+}
